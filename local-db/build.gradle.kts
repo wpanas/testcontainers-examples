a0971cff55a7c00af4a5eb158b7095b83646bb71
Waldemar Panas <8766331+wpanas@users.noreply.github.com>
2021-04-27 19:00:27 +0200

    Upgrade spring boot to 2.4.5 & testcontainers to 1.15.3

--- local-db/build.gradle.kts
+++ local-db/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("org.springframework.boot") version "2.4.4"
+	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("plugin.spring") version "1.4.32"
 	kotlin("plugin.jpa") version "1.4.32"
