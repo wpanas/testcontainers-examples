ef9827d9faf44f2a5c2356fd3ce31f2c21c7912d
wpanas <8766331+wpanas@users.noreply.github.com>
2021-10-15 08:34:40 +0200

    Upgrade kotlin to 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.5.10" apply false
+	kotlin("jvm") version "1.5.31" apply false
 	id("org.jmailen.kotlinter") version "3.4.4"
 }
 
