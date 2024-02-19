3bd4117390ce3b269c48ba62a2e581ca6dcd0c72
Waldemar Panas <waldemar.panas@allegro.com>
2024-02-19 20:26:30 +0100

    Fix Kafka build

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm")
+	kotlin("jvm") version "1.9.22"
 	id("org.springframework.boot") version "2.7.18"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("plugin.spring") version "1.9.22"
